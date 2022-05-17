9bc2e3b00764d6f99f6f108aa2e61562023adc03
TDMWalpala <tharindudarshana2019@gmail.com>
2022-05-17 12:23:07 +0530

    Animated Search Bar

---
+++ Mini Projects/Animated Search Bar/app.js
@@ -0,0 +1,5 @@
+const searchBarConEl = document.querySelector(".search-bar-container");
+const magnifierEl = document.querySelector(".magnifier");
+searchBarConEl.addEventListener("click", ()=>{
+    searchBarConEl.classList.toggle("active");
+});
